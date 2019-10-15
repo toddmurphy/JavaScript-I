362cd48b112f947af68745971ddffa44c3fa1130
toddmurphy <toddmurphy93@gmail.com>
2019-10-14 22:54:45 -0300

    updated stretch goals for converting using map

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -117,15 +117,20 @@ function allCarModels(arr) {
 }
 console.log(allCarModels(inventory))
 
+//FILTER ARRARY HELPER TO CONVERT FROM FOR LOOP TO FILTER - STRETCH GOAL
+const filteredModels = inventory.map(car => car.car_model).sort()
+
+console.log(filteredModels)
+
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = []
 function allCarYears(arr) {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i].car_year) {
-            arr[i].car_year //establishes a true condtion to meet
-            // console.log(arr[i]) //outputs the entire array of inventory
-            // console.log(arr[i].car_year) //outputs just the years in the inventory array
+            //"if" establishes a true condtion to meet
+            //console.log(arr[i]) //outputs the entire array of inventory
+            //console.log(arr[i].car_year) //outputs just the years in the inventory array
             carYears.push(arr[i].car_year)
         }
     }
@@ -134,10 +139,13 @@ function allCarYears(arr) {
 
 console.log(allCarYears(inventory)) //invoke the function and pass in inventory to show years
 
+const mapYears = inventory.map(car => car.car_year)
+console.log(mapYears)
+
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
-let oldCars = inventory.filter(car => car.car_year < 2000)
-console.log(oldCars.length)
+let oldCars = inventory.filter(car => car.car_year < 2000).length
+console.log(oldCars)
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory. Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -8,7 +8,7 @@ myFunction()
 let anotherFunction = param => {
     return param
 }
-anotherFunction('Example')
+console.log(anotherFunction('Example'))
 
 let add = (param1, param2) => {
     return param1 + param2
